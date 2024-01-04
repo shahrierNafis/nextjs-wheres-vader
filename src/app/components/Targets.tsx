a02001d1e1ccfce5ee3f7e4ee66b444ba1df813d
shahrierNafis <102902752+shahrierNafis@users.noreply.github.com>
2024-01-04 08:52:23 +0600

    Refactor: Targets to have animation

--- src/app/components/Targets.tsx
+++ src/app/components/Targets.tsx
@@ -7,24 +7,32 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { motion } from "framer-motion";
 function Targets({ targets }: { targets: Target[] }) {
   return (
     <>
-      <div className="flex">
-        {targets.map((target) => (
-          <TooltipProvider key={target.id}>
-            <Tooltip>
-              <TooltipTrigger>
-                <div key={target.id} className="border shadow rounded">
-                  <Image src={target.imgUrl} alt="" width={50} height={50} />
-                </div>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>{target.name}</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        ))}
+      <div className="flex text-3xl items-center">
+        <span>Targets: </span>
+        <div className="flex">
+          {targets.map((target) => (
+            <TooltipProvider key={target.id}>
+              <Tooltip>
+                <TooltipTrigger>
+                  <motion.div
+                    layout
+                    transition={{ duration: 0.5 }}
+                    className="border shadow rounded "
+                  >
+                    <Image src={target.imgUrl} alt="" width={50} height={50} />
+                  </motion.div>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{target.name}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          ))}
+        </div>
       </div>
     </>
   );
